ab3d9145c091e2a9ca63d6968af2542b3900039e
0-0-1 <doowawisepic@gmail.com>
2016-07-05 14:11:18 -0400

    Created classes

---
+++ src/main/java/co/gm4/GM4_DecorativeMushroom/DecorativeMushroom.java
@@ -0,0 +1,9 @@
+package co.gm4.GM4_DecorativeMushroom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DecorativeMushroom extends JavaPlugin {
+
+	
+	
+}
---
+++ src/main/java/co/gm4/GM4_DecorativeMushroom/events/BookDrop.java
@@ -0,0 +1,5 @@
+package co.gm4.GM4_DecorativeMushroom.events;
+
+public class BookDrop {
+
+}
